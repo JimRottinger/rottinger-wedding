d7097360baac8f0be2b8a9d36c9bcae24f706c9d
Jim Rottinger <jamesrottinger@gmail.com>
2020-04-04 15:47:28 -0400

    Install the prettier formatter

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const fs = require('fs');
-const bodyParser = require('body-parser')
+const express = require("express")
+const app = express()
+const path = require("path")
+const fs = require("fs")
+const bodyParser = require("body-parser")
 
 /** CONFIG **/
-app.use(express.static(__dirname));
+app.use(express.static(__dirname))
 app.use(bodyParser.json())
-app.set('view engine', 'pug');
+app.set("view engine", "pug")
 
 /** STARTUP **/
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
-});
-
-/** ROUTES **/
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
-});
-
-app.get('/*', (req, res) => {
-  res.redirect('/')
+  console.log(`Server listening on port ${PORT}...`)
 })
 
-module.exports = app;
+/** ROUTES **/
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"))
+})
+
+app.get("/*", (req, res) => {
+  res.redirect("/")
+})
+
+module.exports = app
