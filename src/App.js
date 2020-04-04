16d5d2f36e7adf377b01c0349c54418e9521ec8f
Jim Rottinger <jamesrottinger@gmail.com>
2020-04-04 17:15:35 -0400

    Install ant component library

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+import { Row, Col } from 'antd'
+
 const shadowbrookImage = require('./../img/shadowbrook.png')
 
 const RootComponent = () => {
   return (
-    <div className="center-container">
-      <p className="white">James Rottinger & Aileen Saulenas</p>
-      <p className="gold medium serif">3.27.2021</p>
-      <img src={shadowbrookImage} />
-      <p className="small serif">1 Obre Place, Shrewsbury, New Jersey 07702</p>
-    </div>
+    <Row justify="center">
+      <Col align="center">
+        <p className="white">James Rottinger & Aileen Saulenas</p>
+        <p className="gold medium serif">3.27.2021</p>
+        <img src={shadowbrookImage} />
+        <p className="small serif">
+          1 Obre Place, Shrewsbury, New Jersey 07702
+        </p>
+      </Col>
+    </Row>
   )
 }
 
